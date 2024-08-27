9b9fba98cf7bd79a5b0611fe7ee452fce30cf216
Musab Alsayed <musab20151@gmail.com>
2024-08-27 07:56:53 +0300

    add circuls

--- src/Components/CounterOfPins.jsx
+++ src/Components/CounterOfPins.jsx
@@ -7,11 +7,13 @@ function CounterOfPins() {
     const [positionsPins, setPositionsPins] = useState([...positionsObj]);
     const [counter , setCounter] = useState(positionsPins.length);
 
+    // console.log(JSON.parse([...positionsObj]))
+
   return (
     <div className='border-0 shadow-md rounded-lg w-[70%] md:w-[25%] h-[30%] mb-10 flex flex-col justify-evenly items-center  bg-[#CCD5AE]'>
             <h1 className='text-[20px] w-[80%] text-center'>The number of pins in this aerea that request</h1>
                 <div className='border-0 bg-[#E7F0DC] shadow-lg rounded-md w-[30%] md:w-[25%] h-[30%] flex justify-center items-center text-[55px]'>
-                    {counter}
+                    {counter - 1}
                 </div>
     </div>
   )
--- src/Components/MapBox.jsx
+++ src/Components/MapBox.jsx
@@ -1,17 +1,19 @@
 import React, { useRef, useState } from "react";
-import { TileLayer, MapContainer, Popup, Marker } from "react-leaflet";
-import positionsObj from './positionsObj.json'
+import { TileLayer, MapContainer, Popup, Marker, Circle } from "react-leaflet";
+import positionsObj from "./positionsObj.json";
 import mapData from "./MapData";
 import "leaflet/dist/leaflet.css";
-function MapBox() {
 
+function MapBox() {
   const [center, setCenter] = useState([51.505, -0.09]);
   const [positionsPins, setPositionsPins] = useState([...positionsObj]);
+
+  const redOptions = { color: "red" };
 
   const ZOOM_LEVEL = 13;
   const mapRef = useRef();
 
-  console.log(mapRef);
+  console.log(mapRef.current);
 
   return (
     <div className="border-0 w-[80%] md:w-[50%] h-[50%] rounded-2xl shadow-2xl bg-white">
@@ -20,17 +22,19 @@ function MapBox() {
         zoom={ZOOM_LEVEL}
         scrollWheelZoom={false}
         className="w-[100%] h-[100%] rounded-2xl"
-        ref={mapRef}
+        
       >
         <TileLayer
           url={mapData.msptiler.url}
           attribution={mapData.msptiler.attribution}
         />
-        {positionsPins.map((el, index) => (
-          <Marker key={index} position={el.position}>
-            <Popup>{el.title}</Popup>
-          </Marker>
-        ))}
+        <Circle ref={mapRef} center={center} pathOptions={redOptions} radius={1900}>
+          {positionsPins.map((el, index) => (
+            <Marker key={index} position={el.position}>
+              <Popup>{el.title}</Popup>
+            </Marker>
+          ))}
+        </Circle>
       </MapContainer>
     </div>
   );
